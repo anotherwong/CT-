4ea1104ac846213dd165cbf4ec14319267842f19
anotherwong <46949669+anotherwong@users.noreply.github.com>
2019-01-28 12:31:16 +0800

    Update sketch.js

--- p5jsTutorial1.1-master/sketch.js
+++ p5jsTutorial1.1-master/sketch.js
@@ -8,13 +8,18 @@ function setup()
 function draw()
 {
   //create a background using RGB values
-  background(0,0,0)
+  background(90,90,90)
 
   // change the paint brush to a specific color according to RGB values
   fill(0,0,255);
 
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
-  rect(0,0,300,300);
-  rect(300,300,300,300)
+  rect(0,0,100,100);
+  rect(600,400,100,100);
+  rect(0,400,100,100);
+  rect(600,0,100,100);
+  rect(300,200,100,100)
+
+
 
 }
