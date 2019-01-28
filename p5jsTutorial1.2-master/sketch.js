9f9f30f3e1400a4cd8fbe181e1ac727725407255
anotherwong <46949669+anotherwong@users.noreply.github.com>
2019-01-28 13:02:27 +0800

    Add files via upload

---
+++ p5jsTutorial1.2-master/sketch.js
@@ -0,0 +1,30 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(90,90,90)
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the inside color of a shape
+	fill(100,25,255);
+
+	// change the paint brush to a specific color according to RGB values
+	// this color applies only to the outside color of a shape
+	stroke(30,100,15);
+
+	// modifies the thickness of the border of an object
+	strokeWeight(3);
+
+
+	fill(50,255,20);
+	stroke(130,200,15);
+	strokeWeight(20);
+
+	rect(290,100,100,50);
+	rect(150,100,100,50);
+
+}
