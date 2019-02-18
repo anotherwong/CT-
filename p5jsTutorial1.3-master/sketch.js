6736588f54312662bfd4be74c80d8617bb5cd851
anotherwong <46949669+anotherwong@users.noreply.github.com>
2019-02-18 12:37:32 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,28 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+  background(255,139,14)
+
+	fill(0,0,255);
+	stroke(0,0,0);
+	strokeWeight(1);
+
+	text("Hello World",80,30);
+  textSize(20)
+
+	rect(10,10,100,50);
+
+	text("Baby Shark",10,50);
+
+	text("Donald Trump",10,130);
+
+  rect(10,100,150,50);
+
+fill(255,255,255);
+
+}
