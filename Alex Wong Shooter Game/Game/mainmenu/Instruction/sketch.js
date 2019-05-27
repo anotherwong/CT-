151d076efd5740835039ec2134e5039ee89d5d91
anotherwong <46949669+anotherwong@users.noreply.github.com>
2019-05-27 12:51:46 +0800

    Update sketch.js

--- Alex Wong Shooter Game/Game/mainmenu/Instruction/sketch.js
+++ Alex Wong Shooter Game/Game/mainmenu/Instruction/sketch.js
@@ -1,9 +1,8 @@
 //creating a variable called b1 (Stands for button 1)
 var b1;
-
 function setup()
 {
-	createCanvas(500,500);
+	createCanvas(1000,1000);
 
 	//initializing (Set up) button 1 with x y height legnth values
 	b1 = new Button(10,10,100,25);
@@ -16,8 +15,8 @@ function draw()
 {
 	background(255,255,255);
 	text("Use W,A,S,D to move spaceship. Use spacebar to fire. ", 50, 200);
-	text("Do not let the spaceship touch the falling numbers. ", 50, 300);
-
+	text("Do not let the spaceship touch the falling numbers.", 50, 300);
+	text("Shoot the correct numbers in order to get points. If the incorrect answer reaches the bottom, you lose 50 health. ", 50,400)
 	b1.showButton();
 
 	if (b1.getButtonState() == 1)
